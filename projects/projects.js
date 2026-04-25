b0fe1fa74fa6bffc661001e1541ecabc5fd7acf0
Surbiton03 <jookyoungbiz@gmail.com>
2026-04-24 23:29:18 -0700

    Fix duplicate code in projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -6,13 +6,5 @@ const projectsContainer = document.querySelector('.projects');
 
 renderProjects(projects, projectsContainer, 'h2');
 
-import { fetchJSON, renderProjects } from '../global.js';
-
-const projects = await fetchJSON('../lib/projects.json');
-
-const projectsContainer = document.querySelector('.projects');
-
-renderProjects(projects, projectsContainer, 'h2');
-
 const projectsTitle = document.querySelector('.projects-title');
 projectsTitle.textContent = `Projects (${projects.length})`;
