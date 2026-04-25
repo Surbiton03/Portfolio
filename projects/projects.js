00388b3a05bcf2468181cc428eb00eef9e943e77
Surbiton03 <jookyoungbiz@gmail.com>
2026-04-24 17:39:28 -0700

    Add project count to projects page

--- projects/projects.js
+++ projects/projects.js
@@ -5,3 +5,14 @@ const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 
 renderProjects(projects, projectsContainer, 'h2');
+
+import { fetchJSON, renderProjects } from '../global.js';
+
+const projects = await fetchJSON('../lib/projects.json');
+
+const projectsContainer = document.querySelector('.projects');
+
+renderProjects(projects, projectsContainer, 'h2');
+
+const projectsTitle = document.querySelector('.projects-title');
+projectsTitle.textContent = `Projects (${projects.length})`;
